6be88591fe3e66dbda4a74573cf7add7e034a495
Sam <samuelkebbell@gmail.com>
2024-04-15 15:12:08 +0100

    most up to date

--- 1- responsive navigation with html css and js/script.js
+++ 1- responsive navigation with html css and js/script.js
@@ -1,5 +1,8 @@
 const burger = document.querySelector(".burger");
 
 burger.addEventListener("click", () => {
+  const navbar_content = document.querySelector(".navbar.content");
   burger.classList.toggle("active");
+
+  navbar_content.classList.toggle("show");
 });
